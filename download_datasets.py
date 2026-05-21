e3dc31b92ee6764f0613ca9b1cfe586525a4d75b
Pietro <p.valese@studenti.unipi.it>
2026-05-21 09:33:11 +0200

    update download dataset

--- download_datasets.py
+++ download_datasets.py
@@ -1,18 +1,19 @@
-URLS = {
-    "tokyo_xs": "https://drive.google.com/file/d/15QB3VNKj93027UAQWv7pzFQO1JDCdZj2/view?usp=share_link",
-    "sf_xs": "https://drive.google.com/file/d/1tQqEyt3go3vMh4fj_LZrRcahoTbzzH-y/view?usp=share_link",
-    "gsv_xs": "https://drive.google.com/file/d/1q7usSe9_5xV5zTfN-1In4DlmF5ReyU_A/view?usp=share_link",
-    "svox": "https://drive.google.com/file/d/16iuk8voW65GaywNUQlWAbDt6HZzAJ_t9/view?usp=drive_link"
-}
-
 import os
 import gdown
 import shutil
 
 os.makedirs("data", exist_ok=True)
-for dataset_name, url in URLS.items():
+
+URLS = {
+    "tokyo_xs": "15QB3VNKj93027UAQWv7pzFQO1JDCdZj2",
+    "sf_xs":    "1tQqEyt3go3vMh4fj_LZrRcahoTbzzH-y",
+    "gsv_xs":   "1q7usSe9_5xV5zTfN-1In4DlmF5ReyU_A",
+    "svox":     "16iuk8voW65GaywNUQlWAbDt6HZzAJ_t9",
+}
+
+for dataset_name, file_id in URLS.items():
     print(f"Downloading {dataset_name}")
     zip_filepath = f"data/{dataset_name}.zip"
-    gdown.download(url, zip_filepath, fuzzy=True)
+    gdown.download(id=file_id, output=zip_filepath, quiet=False)
     shutil.unpack_archive(zip_filepath, extract_dir="data")
     os.remove(zip_filepath)
